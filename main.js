d7c58596ac60de0630a6412818451d56f1179ffb
sifatbbillah <sifatbillah82@gmail.com>
2026-08-09 19:54:32 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum: " + sum);
+    }
+}
